ced1ef33b342d88e25962b4a5bb8ab5325af1c6a
Erik Juhlin <erik.juhlin@tretton37.com>
2019-06-10 10:28:06 +0200

    Added AkamaiApiAuth project

---
+++ AkamaiApiAuth/AkamaiAuthHttpClientHandler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Net.Http.Headers;
+using MediaTypeHeaderValue = System.Net.Http.Headers.MediaTypeHeaderValue;
+
+namespace AkamaiApiAuth
+{
+    public class AkamaiAuthHttpClientHandler : DelegatingHandler
+    {
+        private readonly AkamaiAuthOptions _options;
+        private readonly AkamaiAuthGenerator _akamaiAuthGenerator;
+
+        public AkamaiAuthHttpClientHandler(AkamaiAuthOptions options)
+        {
+            _options = options ?? throw new ArgumentNullException(nameof(options));
+            _akamaiAuthGenerator = new AkamaiAuthGenerator(options);
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request.Content != null && !string.IsNullOrEmpty(_options.RequestContentType))
+            {
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse(_options.RequestContentType);
+            }
+
+            await SignRequest(request);
+            var response = await base.SendAsync(request, cancellationToken);
+            ValidateResponse(response);
+
+            return response;
+        }
+
+        private async Task SignRequest(HttpRequestMessage request)
+        {
+            request.Headers.Remove(HeaderNames.Authorization);
+            var authHeader = await _akamaiAuthGenerator.Generate(request);
+            request.Headers.Add(HeaderNames.Authorization, authHeader);
+        }
+
+        private static void ValidateResponse(HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                return;
+            }
+
+            if (!response.Headers.TryGetValues(HeaderNames.Date, out var values))
+            {
+                return;
+            }
+
+            var value = values.FirstOrDefault();
+            if (value == null || DateTime.TryParse(value, out var date))
+            {
+                return;
+            }
+
+            var diff = (DateTime.UtcNow - date).TotalSeconds;
+            if (Math.Abs(diff) > 30)
+            {
+                throw new Exception("Local server date is more than 30s out of sync with remote server");
+            }
+        }
+    }
+}
---
+++ AkamaiApiAuth/AkamaiAuthOptions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace AkamaiApiAuth
+{
+    public class AkamaiAuthOptions
+    {
+        public string ClientToken { get; set; }
+        public string AccessToken { get; set; }
+        public string ClientSecret { get; set; }
+        public string RequestContentType { get; set; } = "application/json";
+        public IList<string> IncludeHeaders { get; set; }
+        public int? MaxBodyHashSize { get; set; }
+    }
+}
